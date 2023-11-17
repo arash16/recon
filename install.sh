533b39a8dad6a7fabbc20362ed785f2457911526
arash16 <arash.shakery@gmail.com>
2023-11-17 17:36:42 +0330

    lighter

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-echo $1 >> /root/Tools/.github_tokens
+echo $1 > /root/Tools/.github_tokens
 
 curl -L https://github.com/trufflesecurity/trufflehog/releases/download/v3.62.1/trufflehog_3.62.1_linux_amd64.tar.gz -o trufflehog.tar.gz
 tar -xf trufflehog.tar.gz
