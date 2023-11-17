fe20ddc0d7f4e195b1f4ec15009ef3a075d5ab56
arash16 <arash.shakery@gmail.com>
2023-11-17 17:17:30 +0330

    fix

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+echo $1 >> /root/Tools/.github_tokens
+
 curl -L https://github.com/trufflesecurity/trufflehog/releases/download/v3.62.1/trufflehog_3.62.1_linux_amd64.tar.gz -o trufflehog.tar.gz
 tar -xf trufflehog.tar.gz
 chmod +x trufflehog
