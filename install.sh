795eb6ae3961c28f5b5fb18c4f851a9b2a0298c7
arash16 <arash.shakery@gmail.com>
2023-11-17 16:31:25 +0330

    fix issues

---
+++ install.sh
@@ -0,0 +1,4 @@
+curl -L https://github.com/trufflesecurity/trufflehog/releases/download/v3.62.1/trufflehog_3.62.1_linux_amd64.tar.gz -o trufflehog.tar.gz
+tar -xf trufflehog.tar.gz
+chmod +x trufflehog
+mv trufflehog /usr/local/bin/
